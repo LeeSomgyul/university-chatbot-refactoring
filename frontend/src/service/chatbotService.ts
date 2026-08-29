4cdaabdc06357058de257f9a92b08c0147137f56
a-neun <153486449+eunhye-ahn@users.noreply.github.com>
2026-08-30 00:33:23 +0900

    chore: 오타 수정

--- frontend/src/service/chatbotService.ts
+++ frontend/src/service/chatbotService.ts
@@ -6,7 +6,7 @@ interface ChatbotResponse {
     session_id: string;             // 세션 ID
     sections?: {                 // 식도락
         keyword: string;
-        review_mathced: boolean;
+        review_matched: boolean;
         restaurants: {
             name: string;
             address: string;
